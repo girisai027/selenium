bd8daa7abeab8bd54ae41469f971d69f8d99218a
girisai027 <girisai027@gmail.com>
2024-06-28 16:23:41 +0530

    Updated Code

--- src/practice/DemoGit.java
+++ src/practice/DemoGit.java
@@ -1,14 +1,20 @@
 package practice;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class DemoGit {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void facebook() {
+	  driver.get("https://www.facebook.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
